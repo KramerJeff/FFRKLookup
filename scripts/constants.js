46e96c1bdeab836fd1ad1b32b189e8f310b0718c
Jeff Kramer <kramer.a.jeffrey@gmail.com>
2019-11-04 08:29:55 -0500

    Fix Enna Kros alias

--- scripts/constants.js
+++ scripts/constants.js
@@ -93,7 +93,7 @@ export const characterAliases = {
   "red xiii": ["nanaki", "red13", "red 13"],
   "gogo (v)": ["gogo v", "gogo5", "gogo 5"],
   "gogo (vi)": ["gogo vi", "gogo6", "gogo 6"],
-  "enna": ["enna kros"]
+  "enna kros": ["enna"]
 };
 
 export const abilityAliases = {
